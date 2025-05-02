74c34e19f45491123cbcafb4a5cce11c18a357b1
eepymeowers System <106939391+eepymeowers@users.noreply.github.com>
2025-05-01 23:27:01 -0400

    Add files via upload

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,120 +1,65 @@
-#
-# Copyright (C) 2023 The LineageOS Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 DEVICE_PATH := device/amazon/mustang
+KERNEL_PATH := kernel/amazon/mustang
+VENDOR_PATH := vendor/amazon/mustang
 
-# APEX
-OVERRIDE_TARGET_FLATTEN_APEX := true
+# headers
+TARGET_SPECIFIC_HEADER_PATH := $(DEVICE_PATH)/include
 
-# Architecture
-TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv8-a
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_VARIANT := cortex-a53
-TARGET_CPU_VARIANT_RUNTIME := cortex-a53
-
-# A/B
-AB_OTA_UPDATER := false
-
-# Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := mustang
-TARGET_NO_BOOTLOADER := true
-
-# Boot Image
-BOARD_BOOTIMG_HEADER_VERSION := 1
-BOARD_RAMDISK_USE_XZ := true
-BOARD_KERNEL_BASE         := 0x40080000
-BOARD_KERNEL_PAGESIZE     := 2048
-BOARD_KERNEL_OFFSET       := 0x00000000
-BOARD_RAMDISK_OFFSET      := 0x03400000
-BOARD_KERNEL_TAGS_OFFSET  := 0x07f80000
-BOARD_SECOND_OFFSET       := 0x00e80000
-BOARD_MKBOOTIMG_ARGS := \
-    --kernel_offset $(BOARD_KERNEL_OFFSET) \
-    --ramdisk_offset $(BOARD_RAMDISK_OFFSET) \
-    --second_offset $(BOARD_SECOND_OFFSET)   \
-    --tags_offset $(BOARD_KERNEL_TAGS_OFFSET) \
-    --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
-
-BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
-BOARD_KERNEL_CMDLINE += loop.max_part=7
-BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware
-BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
-
-# Build System
-BUILD_BROKEN_DUP_RULES := true
-BUILD_BROKEN_PREBUILT_ELF_FILES := true
-BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
-
-# Display
-TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS := 0x00000200
-
-# Filesystems
-BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE   := ext4
-BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE    := ext4
-BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE   := ext4
-TARGET_USERIMAGES_USE_EXT4 := true
-
-# HIDL
-DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
-DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/compatibility_matrix.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/device_framework_compatibility_matrix.xml
-
-# HWUI
-HWUI_COMPILE_FOR_PERF := true
-
-# Kernel
-BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
-TARGET_KERNEL_ARCH := arm64
-TARGET_KERNEL_SOURCE := kernel/amazon/mustang
-TARGET_KERNEL_CONFIG := lineageos_mustang_defconfig
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := $(shell pwd)/prebuilts/linaro/linux-x86/aarch64/aarch64-linux-gnu/bin/aarch64-linux-gnu-
-TARGET_KERNEL_CLANG_COMPILE := false
-
-# Malloc
-MALLOC_SVELTE := true
-
-# Partitions
-BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 20971520
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3151745024
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 11633933824
-BOARD_CACHEIMAGE_PARTITION_SIZE := 499261440
-BOARD_VENDORIMAGE_PARTITION_SIZE := 224350208
-BOARD_FLASH_BLOCK_SIZE := 131072
-
-# Vendor
-TARGET_COPY_OUT_VENDOR := vendor
-BOARD_USES_VENDORIMAGE := true
+# inherit from the proprietary version
+-include $(VENDOR_PATH)/BoardConfigVendor.mk
 
 # Platform
 TARGET_BOARD_PLATFORM := mt8163
+TARGET_BOARD_PLATFORM_GPU := mali-720mp2
 
-# Properties
-TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
-TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
-# Recovery
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.mt8163
+# Bootloader
+TARGET_NO_BOOTLOADER := true
 
-# Display
-TARGET_SCREEN_DENSITY := 171
+# Architecture
+TARGET_ARCH := arm
+TARGET_CPU_VARIANT := cortex-a7
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT_CPU := cortex-a7
+TARGET_CPU_VARIANT:= cortex-a7
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := true
 
-# Verified Boot
-BOARD_AVB_ENABLE := false
+# Kernel Config
+MTK_APPENDED_DTB_SUPPORT := true
+BOARD_KERNEL_BASE := 0x40080000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_OFFSET := 0x00000000
+BOARD_KERNEL_TAGS_OFFSET := 0x07f80000
+BOARD_RAMDISK_OFFSET := 0x03400000
+BOARD_SECOND_OFFSET := 0x00e80000
 
-# VNDK
-BOARD_VNDK_VERSION := current
+BOARD_MKBOOTIMG_ARGS := \
+    --kernel_offset $(BOARD_KERNEL_OFFSET) \
+    --ramdisk_offset $(BOARD_RAMDISK_OFFSET) \
+    --tags_offset $(BOARD_KERNEL_TAGS_OFFSET) \
+    --second_offset $(BOARD_SECOND_OFFSET)
 
-# BT
-BOARD_HAS_BLUETOOTH := true
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
+BOARD_KERNEL_IMAGE_NAME := zImage-dtb
+TARGET_KERNEL_SOURCE := $(KERNEL_PATH)
+KERNEL_TOOLCHAIN_PREFIX := $(shell pwd)/prebuilts/gcc/linux-x86/arm/gcc-linaro-6.3.1-2017.05-x86_64_arm-eabi/bin/arm-eabi-
+
+TARGET_KERNEL_CONFIG := mustang_defconfig
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+
+BOARD_HAS_MTK_HARDWARE := true
+
+# Build a separate vendor.img
+TARGET_COPY_OUT_VENDOR := vendor
+BOARD_NEEDS_VENDORIMAGE_SYMLINK := true
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+
+# Binder API version
+TARGET_USES_64_BIT_BINDER := true
+
+BOARD_NO_SECURE_DISCARD := true
 
 # WIFI
 BOARD_WLAN_DEVICE := MediaTek
@@ -124,15 +69,83 @@ BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_mt66xx
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_mt66xx
 WIFI_DRIVER_FW_PATH_PARAM := "/dev/wmtWifi"
-WIFI_DRIVER_FW_PATH_STA:="STA"
-WIFI_DRIVER_FW_PATH_AP:="AP"
-WIFI_DRIVER_FW_PATH_P2P:="P2P"
-WIFI_DRIVER_STATE_CTRL_PARAM := "/dev/wmtWifi"
-WIFI_DRIVER_STATE_ON := "1"
-WIFI_DRIVER_STATE_OFF := "0"
-WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
+WIFI_DRIVER_FW_PATH_STA:=STA
+WIFI_DRIVER_FW_PATH_AP:=AP
+WIFI_DRIVER_FW_PATH_P2P:=P2P
+WIFI_DRIVER_STATE_CTRL_PARAM := /dev/wmtWifi
+WIFI_DRIVER_STATE_ON := 1
+WIFI_DRIVER_STATE_OFF := 0
 
-# Inherit the proprietary files
-include vendor/amazon/mustang/BoardConfigVendor.mk
-include vendor/amazon/mt8163/BoardConfigVendor.mk
-include device/mediatek/sepolicy_vndr/SEPolicy.mk
+# Audio
+USE_XML_AUDIO_POLICY_CONF := 1
+USE_CUSTOM_AUDIO_POLICY := 1
+
+# BT
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
+BOARD_HAVE_BLUETOOTH := true
+
+# Graphics
+BOARD_EGL_CFG := $(DEVICE_PATH)/configs/egl.cfg
+USE_OPENGL_RENDERER := true
+VSYNC_EVENT_PHASE_OFFSET_NS := 0
+TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+
+vendor_lkm_dir := $(DEVICE_PATH)/lkm-4.9
+BOARD_VENDOR_KERNEL_MODULES := \
+  $(vendor_lkm_dir)/bt_drv.ko \
+  $(vendor_lkm_dir)/wlan_drv_gen2.ko \
+  $(vendor_lkm_dir)/wmt_chrdev_wifi.ko \
+  $(vendor_lkm_dir)/wmt_drv.ko \
+  $(vendor_lkm_dir)/br_netfilter.ko \
+  $(vendor_lkm_dir)/xt_physdev.ko
+
+# Filesystem
+BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 20971520
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3151745024
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 11633933824
+BOARD_CACHEIMAGE_PARTITION_SIZE := 499261440
+BOARD_VENDORIMAGE_PARTITION_SIZE := 224350208
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+TARGET_USERIMAGES_USE_EXT4 := true
+
+BLOCK_BASED_OTA := true
+
+# Manifest
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/compatibility_matrix.xml
+
+# Seccomp filter
+BOARD_SECCOMP_POLICY += $(DEVICE_PATH)/seccomp
+
+# SELinux
+#BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+
+# Vold
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/mt_usb/musb-hdrc.0.auto/gadget/lun%d/file
+
+# TWRP
+ifneq (,$(strip $(wildcard bootable/recovery-twrp/twrp.cpp)))
+RECOVERY_VARIANT := twrp
+endif
+DEVICE_RESOLUTION := 600x1024
+TW_EXCLUDE_MTP := false
+RECOVERY_SDCARD_ON_DATA := true
+TW_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness
+TARGET_RECOVERY_LCD_BACKLIGHT_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+RECOVERY_FSTAB_VERSION := 2
+TW_THEME := landscape_mdpi
+#TWRP_EVENT_LOGGING := true
+RECOVERY_TOUCHSCREEN_SWAP_XY := true
+#RECOVERY_GRAPHICS_USE_LINELENGTH := true
+#RECOVERY_TOUCHSCREEN_FLIP_Y := true 
+RECOVERY_TOUCHSCREEN_FLIP_X := true
+BOARD_HAS_FLIPPED_SCREEN := true
+
+MALLOC_SVELTE := true
+
+# Disable API check
+WITHOUT_CHECK_API := true
